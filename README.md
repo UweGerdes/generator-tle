e71f44ad5db9838137f34bb02fd01669b42cb8d3
Uwe Gerdes <entwicklung@uwegerdes.de>
2017-06-16 12:13:17 +0200

    initial generator working

---
+++ README.md
@@ -0,0 +1,7 @@
+# Yeoman generator ez54
+
+If using from a Docker container be sure to have this generator in /home/node/src and the target directory in /home/node/app. The node_modules are in /home/node, more dependencies should be installed there.
+
+TODO: make sure the Dockerfile creates a usable node_modules environment
+TODO: workflow for adding node modules to the container and Dockerfile / package.json
+
---
+++ generators/app/index.js
@@ -0,0 +1,62 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument('modulename', { type: String, required: false });
+  }
+
+  initializing() {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the ' + chalk.yellow('generator-ez54') + ' generator!'
+    ));
+  }
+
+  prompting() {
+    const prompts = [{
+      type    : 'input',
+      name    : 'name',
+      message : 'Your project name',
+      default : this.appname // Default to current folder name
+    }, {
+      type    : 'input',
+      name    : 'module',
+      message : 'New module name',
+      default : this.options.modulename // Default to first argument
+    }, {
+      type    : 'confirm',
+      name    : 'someAnswer',
+      message : 'Would you like to enable this option?',
+      default : true
+    }];
+
+    return this.prompt(prompts).then(props => {
+      // To access props later use this.props.<name>;
+      this.props = props;
+    });
+  }
+
+  writing() {
+    this.fs.copy(
+      this.templatePath('dummyfile.txt'),
+      this.destinationPath(path.join(this.props.name, this.props.module + '.txt')
+    );
+  }
+
+  install() {
+//    this.installDependencies();
+    this.log(chalk.red('not installing generator-ez54 generator!'));
+  }
+
+  end() {
+    this.log(yosay(
+      'finished generating ' + chalk.green('generator-ez54') + '.'
+    ));
+  }
+};
